f8fd8916ecda6563b1c1116a55b435b6325a4bf0
thefoku <leekengham@gmail.com>
2024-12-26 18:45:55 +0400

    feat: solve Carbon Dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,18 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if ( (typeof sampleActivity !== 'string') 
+    || (typeof (sampleActivity * 1) !== 'number') 
+    || isNaN(sampleActivity * 1)
+    || sampleActivity <= 0
+    || sampleActivity > 15
+  ) {
+  return false;
+}
+  const k = Math.log(2) / HALF_LIFE_PERIOD;
+  const time = Math.log(MODERN_ACTIVITY / sampleActivity) / k;
+  return Math.ceil(time);
 }
 
 module.exports = {
