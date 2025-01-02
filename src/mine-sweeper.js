bab3d7e6273cfd621764f9d3fd66af4487f86621
thefoku <leekengham@gmail.com>
2025-01-02 18:39:21 +0400

    feat: Mine Sweeper task solve

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,11 +23,39 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  console.log(matrix)
+  return matrix.map((first, indFrst) => {
+    const topRow = matrix[indFrst - 1];
+    const botRow = matrix[indFrst + 1];
+    return first.map((elem, indElem) => {
+      const prevElem = first[indElem - 1];
+      const nextElem = first[indElem + 1];
+      if (elem === true) {
+        return 1;
+      }
+      if((
+         (topRow && topRow[indElem] === true) || // верхний
+         (botRow && botRow[indElem] === true)) && // нижний и
+         (prevElem === true || nextElem === true) // лево или право
+        ) {
+        return 2;
+      }
+      if (
+       (prevElem === true || nextElem === true) ||
+       ((topRow && topRow[indElem - 1] === true) ||
+        (topRow && topRow[indElem + 1] === true) ||
+         (botRow && botRow[indElem  - 1] === true) ||
+       (botRow && botRow[indElem + 1] === true)) ||
+        (topRow && topRow[indElem] === true) ||
+         (botRow && botRow[indElem] === true)
+         ) {
+        return 1;
+      }
+      return 0;
+    })
+  })
 }
-
 module.exports = {
   minesweeper
 };
